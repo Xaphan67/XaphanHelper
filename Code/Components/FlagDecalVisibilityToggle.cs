d4896eecf5e7f68e33a57e7a887dae942a476d84
Xaphan67 <cedric.falda@gmail.com>
2023-08-10 01:58:22 +0200

    fix: Added missing components for custom decal registry properties

---
+++ Code/Components/FlagDecalVisibilityToggle.cs
@@ -0,0 +1,30 @@
+﻿using Monocle;
+
+namespace Celeste.Mod.XaphanHelper.Components
+{
+    public class FlagDecalVisibilityToggle : Component
+    {
+        public string[] Flags;
+
+        public bool Inverted;
+
+        public FlagDecalVisibilityToggle(string[] flags, bool inverted) : base(true, false)
+        {
+            Flags = flags;
+            Inverted = inverted;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            foreach (string flag in Flags)
+            {
+                Entity.Visible = Inverted ? Entity.SceneAs<Level>().Session.GetFlag(flag) : !Entity.SceneAs<Level>().Session.GetFlag(flag);
+                if (Entity.Visible)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Code/Components/RandomFlagSwapImage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Monocle;
+
+namespace Celeste.Mod.XaphanHelper.Components
+{
+    public class RandomFlagSwapImage : Component
+    {
+        private string flag;
+        private List<MTexture> off;
+        private List<MTexture> on;
+        private MTexture currentTexture;
+        private bool prevState;
+        public RandomFlagSwapImage(string flag, List<MTexture> off, List<MTexture> on) : base(active: true, visible: true)
+        {
+            this.flag = flag;
+            this.off = off;
+            this.on = on;
+            SelectTexture(prevState);
+        }
+
+        public override void Added(Entity entity)
+        {
+            base.Added(entity);
+            prevState = (Scene as Level).Session.GetFlag(flag);
+            SelectTexture(prevState);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            bool state = (Scene as Level).Session.GetFlag(flag);
+            if (state != prevState)
+            {
+                SelectTexture(state);
+            }
+            prevState = state;
+        }
+
+        public override void Render()
+        {
+            Decal decal = Entity as Decal;
+            currentTexture.DrawCentered(decal.Position, decal.Color, decal.Scale, decal.Rotation);
+        }
+
+        private void SelectTexture(bool state)
+        {
+            var list = state ? on : off;
+            currentTexture = list[Calc.Random.Next(list.Count)];
+        }
+    }
+}
