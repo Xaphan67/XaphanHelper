3af62590dcb2187926c9b67a93258c16d985d8a2
Xaphan67 <cedric.falda@gmail.com>
2024-06-29 01:16:13 +0200

    feature: Dynamite

---
+++ Code/Entities/Celeste/Dynamite.cs
@@ -0,0 +1,36 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.XaphanHelper.Entities
+{
+    [Tracked(true)]
+    [CustomEntity("XaphanHelper/Dynamite")]
+    class Dynamite : Entity
+    {
+        private MTexture Texture;
+
+        public Dynamite(EntityData data, Vector2 position) : base(data.Position + position)
+        {
+            Tag = Tags.TransitionUpdate;
+            Collider = new Hitbox(10f, 10f, -1f, -1f);
+            Texture = GFX.Game["objects/XaphanHelper/Dynamite/dynamite"];
+            Depth = -20000;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (!CollideCheck<Solid>())
+            {
+                RemoveSelf();
+            }
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            Texture.Draw(Position - new Vector2(4f, 4f));
+        }
+    }
+}
