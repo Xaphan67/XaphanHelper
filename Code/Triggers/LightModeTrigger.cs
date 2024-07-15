885b979c1daaea9e2056a4f6610507c2cce2d206
Xaphan67 <cedric.falda@gmail.com>
2024-07-15 17:09:01 +0200

    feature: Light Mode Trigger

---
+++ Code/Triggers/LightModeTrigger.cs
@@ -0,0 +1,37 @@
+﻿using Celeste.Mod.Entities;
+using Celeste.Mod.XaphanHelper.Managers;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.XaphanHelper.Triggers
+{
+    [CustomEntity("XaphanHelper/LightModeTrigger")]
+    class LightModeTrigger : Trigger
+    {
+        private enum Modes
+        {
+            Light,
+            Dark,
+            None
+        }
+
+        private readonly Modes mode;
+
+        public LightModeTrigger(EntityData data, Vector2 offset) : base(data, offset)
+        {
+            mode = data.Enum("mode", Modes.None);
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            Level level = Scene as Level;
+            foreach (LightManager manager in level.Tracker.GetEntities<LightManager>())
+            {
+                manager.MainMode = manager.RespawnMode = (XaphanModuleSession.LightModes)mode;
+                Logger.Log(LogLevel.Info, "XH", "Change light mode to " + (XaphanModuleSession.LightModes)mode);
+            }
+            XaphanModuleSession.LightModes lightMode = XaphanModuleSession.LightModes.None;
+            lightMode = (XaphanModuleSession.LightModes)mode;
+        }
+    }
+}
