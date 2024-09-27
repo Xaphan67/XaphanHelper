30f9ffe32985ab36df36cd5793a65a0f9841f54b
Xaphan67 <cedric.falda@gmail.com>
2024-09-27 02:17:18 +0200

    feature: Thorns barriers destroy nearby barriers and visual improvements

--- Code/Entities/Celeste/ThornBarrier.cs
+++ Code/Entities/Celeste/ThornBarrier.cs
@@ -6,9 +6,56 @@ using Monocle;
 
 namespace Celeste.Mod.XaphanHelper.Entities
 {
+    [Tracked(true)]
     [CustomEntity("XaphanHelper/ThornBarrier")]
     class ThornBarrier : Entity
     {
+        private class Border : Entity
+        {
+            private Entity drawing = new();
+
+            public Border(Entity parent)
+            {
+                drawing = parent;
+                Depth = parent.Depth + 2;
+            }
+
+            public override void Render()
+            {
+                if (drawing.Visible)
+                {
+                    DrawBorder(drawing);
+                }
+            }
+
+            private void DrawBorder(Entity entity)
+            {
+                if (entity != null)
+                {
+                    foreach (Component component in entity.Components)
+                    {
+                        Sprite sprite = component as Sprite;
+                        if (sprite != null)
+                        {
+                            Color color = sprite.Color;
+                            Vector2 position = sprite.Position;
+                            sprite.Color = Color.Black;
+                            sprite.Position = position + new Vector2(0f, -1f);
+                            sprite.Render();
+                            sprite.Position = position + new Vector2(0f, 1f);
+                            sprite.Render();
+                            sprite.Position = position + new Vector2(-1f, 0f);
+                            sprite.Render();
+                            sprite.Position = position + new Vector2(1f, 0f);
+                            sprite.Render();
+                            sprite.Color = color;
+                            sprite.Position = position;
+                        }
+                    }
+                }
+            }
+        }
+
         [Pooled]
         public class ThornDebris : Actor
         {
@@ -118,15 +165,22 @@ namespace Celeste.Mod.XaphanHelper.Entities
                 }
             }
         }
-        
+
+        private Border border;
+
         private Sprite Sprite;
 
         public string Directory;
 
         private string flag;
 
-        public ThornBarrier(EntityData data, Vector2 offset) : base(data.Position + offset)
+        private int group;
+
+        EntityID ID;
+
+        public ThornBarrier(EntityData data, Vector2 offset, EntityID id) : base(data.Position + offset)
         {
+            ID = id;
             Tag = Tags.TransitionUpdate;
             Collider = new Circle(10f);
             flag = data.Attr("flag");
@@ -140,6 +194,51 @@ namespace Celeste.Mod.XaphanHelper.Entities
             Sprite.AddLoop("light", "light", 0.08f);
             Sprite.AddLoop("dark", "dark", 0.08f);
             Sprite.CenterOrigin();
+            int rotationIndex = Calc.Random.Range(0, 8);
+            switch (rotationIndex)
+            {
+                case 1:
+                    Sprite.Rotation = (float)Math.PI / 4f;
+                    break;
+                case 2:
+                    Sprite.Rotation = (float)Math.PI / 2;
+                    break;
+                case 3:
+                    Sprite.Rotation = (float)Math.PI / 4f * 3f;
+                    break;
+                case 4:
+                    Sprite.Rotation = (float)Math.PI;
+                    break;
+                case 5:
+                    Sprite.Rotation = -(float)Math.PI / 4f;
+                    break;
+                case 6:
+                    Sprite.Rotation = -(float)Math.PI / 2f;
+                    break;
+                case 7:
+                    Sprite.Rotation = -(float)Math.PI / 4f * 3f;
+                    break;
+            }
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+            foreach (ThornBarrier barrier in SceneAs<Level>().Tracker.GetEntities<ThornBarrier>())
+            {
+                if (barrier != this && CollideCheck(barrier))
+                {
+                    if (barrier.group != 0)
+                    {
+                        group = barrier.group;
+                    }
+                }
+            }
+            if (group == 0)
+            {
+                group = ID.ID;
+            }
+            Scene.Add(border = new Border(this));
         }
 
         private void onPlayer(Player player)
@@ -177,11 +276,30 @@ namespace Celeste.Mod.XaphanHelper.Entities
             }
         }
 
-        public void Destroy()
+        public void Destroy(bool all = true)
         {
             Audio.Play("event:/game/06_reflection/fall_spike_smash", Position);
-            ThornDebris.Burst(Position, 16);
+            ThornDebris.Burst(Position, 8);
+            if (all)
+            {
+                foreach (ThornBarrier barrier in SceneAs<Level>().Tracker.GetEntities<ThornBarrier>())
+                {
+                    if (barrier != this && barrier.group == group)
+                    {
+                        barrier.Destroy(false);
+                    }
+                }
+            }
             RemoveSelf();
+        }
+
+        public override void Removed(Scene scene)
+        {
+            base.Removed(scene);
+            if (border != null && border.Scene == scene)
+            {
+                border.RemoveSelf();
+            }
         }
     }
 }
