0f51da3f928076ad6c441cb9d01b6a0eae541441
AAA1459 <72622056+AAA1459@users.noreply.github.com>
2025-07-29 11:56:18 +0800

    Add files via upload

--- Code/Managers/ScrewAttackManager.cs
+++ Code/Managers/ScrewAttackManager.cs
@@ -162,10 +162,10 @@ namespace Celeste.Mod.XaphanHelper.Managers
                 }
                 PlayerSprite.Color = player.Sprite.Color;
                 Position = player.Position + new Vector2(0f, -4f);
-                PlayerSprite.FlipX = player.Facing == Facings.Left ? true : false;
-                PlayerHairSprite.FlipX = player.Facing == Facings.Left ? true : false;
-                ScrewAttackSprite.FlipX = player.Facing == Facings.Left ? true : false;
-                if (((player.Sprite.CurrentAnimationID.Contains("jumpFast") || StartedScrewAttack) && player.StateMachine.State == 0 && !player.DashAttacking && !player.OnGround() && player.Holding == null && !SceneAs<Level>().Session.GetFlag("Xaphan_Helper_Ceiling") && !XaphanModule.PlayerIsControllingRemoteDrone() && (GravityJacket.determineIfInLiquid() ? GravityJacket.Active(SceneAs<Level>()) : true)) && !player.Sprite.CurrentAnimationID.Contains("slide") && Math.Abs(player.Speed.X) >= 90 && !CannotScrewAttack)
+                PlayerSprite.FlipX = player.Facing == Facings.Left;
+                PlayerHairSprite.FlipX = player.Facing == Facings.Left;
+                ScrewAttackSprite.FlipX = player.Facing == Facings.Left;
+                if (((player.fastJump && player.Speed.Y < 0f) || StartedScrewAttack) && player.StateMachine.State == 0 && !player.DashAttacking && !player.OnGround() && player.Holding == null && !SceneAs<Level>().Session.GetFlag("Xaphan_Helper_Ceiling") && !XaphanModule.PlayerIsControllingRemoteDrone() && (GravityJacket.determineIfInLiquid() ? GravityJacket.Active(SceneAs<Level>()) : true) && Math.Abs(player.Speed.X) >= 90 && !CannotScrewAttack)
                 {
                     if (!screwAttackSfx.Playing && !SceneAs<Level>().Frozen)
                     {
@@ -216,7 +216,7 @@ namespace Celeste.Mod.XaphanHelper.Managers
                             }
                         }
                     }
-                    if (!XaphanModule.useMetroidGameplay)
+                    if (XaphanModule.useMetroidGameplay)
                     {
                         foreach (Enemy enemy in Scene.Tracker.GetEntities<Enemy>())
                         {
