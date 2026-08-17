f766227368b58f3a44dfc10e12baaebb9877a259
Xaphan67 <cedric.falda@gmail.com>
2026-08-17 03:10:12 +0200

    fix: Light Mode Trigger correctly set mode to none + improved its code

--- Code/Triggers/LightModeTrigger.cs
+++ Code/Triggers/LightModeTrigger.cs
@@ -1,47 +1,29 @@
-﻿using System.Collections.Generic;
-using Celeste.Mod.Entities;
+﻿using Celeste.Mod.Entities;
 using Celeste.Mod.XaphanHelper.Managers;
 using Microsoft.Xna.Framework;
+using static Celeste.Mod.XaphanHelper.XaphanModuleSession;
 
 namespace Celeste.Mod.XaphanHelper.Triggers
 {
     [CustomEntity("XaphanHelper/LightModeTrigger")]
     class LightModeTrigger : Trigger
     {
-        private enum Modes
-        {
-            Light,
-            Dark,
-            None
-        }
-
-        private readonly Modes mode;
+        private readonly LightModes mode;
 
         public LightModeTrigger(EntityData data, Vector2 offset) : base(data, offset)
         {
-            mode = data.Enum("mode", Modes.None);
+            mode = data.Enum("mode", LightModes.None);
         }
 
         public override void OnEnter(Player player)
         {
             base.OnEnter(player);
             Level level = Scene as Level;
-            List<LightManager> lights = new List<LightManager>();
             foreach (LightManager manager in level.Tracker.GetEntities<LightManager>())
             {
-                manager.MainMode = manager.RespawnMode = (XaphanModuleSession.LightModes)mode;
-                if (mode == Modes.None)
-                {
-                    lights.Add(manager);
-                }
+                manager.MainMode = manager.RespawnMode = mode;
             }
-            foreach (LightManager manager in lights)
-            {
-                manager.RemoveSelf();
-            }
-            lights.Clear();
-            XaphanModuleSession.LightModes lightMode = XaphanModuleSession.LightModes.None;
-            lightMode = (XaphanModuleSession.LightModes)mode;
+            XaphanModule.ModSession.LightMode = mode;
         }
     }
 }
