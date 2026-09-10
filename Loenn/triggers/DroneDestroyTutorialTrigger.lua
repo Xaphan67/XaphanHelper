491563dd0a6fdd4f40fbb3f7a2e56ebc1eaf4ef6
Xaphan67 <cedric.falda@gmail.com>
2026-09-10 20:12:44 +0200

    feature: Drone Destroy Tutorial Trigger plugin

---
+++ Loenn/triggers/DroneDestroyTutorialTrigger.lua
@@ -0,0 +1,12 @@
+local DroneDestroyTutorialTrigger = {}
+
+DroneDestroyTutorialTrigger.name = "XaphanHelper/DroneDestroyTutorialTrigger"
+DroneDestroyTutorialTrigger.placements = {
+    name = "DroneDestroyTutorialTrigger",
+    data = {
+        flag = "",
+        onlyOnce =false
+    }
+}
+
+return DroneDestroyTutorialTrigger
