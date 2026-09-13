6a6999d77806e2e06d011454bdedd36371aa6a75
Xaphan67 <cedric.falda@gmail.com>
2026-09-14 01:55:37 +0200

    feature: FG Tiles Jumpthru plugin

---
+++ Loenn/entities/FGTilesJumpthru.lua
@@ -0,0 +1,76 @@
+local drawableSpriteStruct = require("structs.drawable_sprite")
+local drawing = require("utils.drawing")
+local utils = require("utils")
+local enums = require("consts.celeste_enums")
+
+local FGTilesJumpthru = {}
+
+FGTilesJumpthru.name = "XaphanHelper/FGTilesJumpthru"
+FGTilesJumpthru.depth = -9000
+FGTilesJumpthru.canResize = {true, false}
+FGTilesJumpthru.fieldInformation = {
+    texture = {
+        options = {"wood", "dream", "temple", "templeB", "cliffside", "reflection", "core", "moon"},
+        editable = true
+    },
+    surfaceIndex = {
+        options = enums.tileset_sound_ids,
+        fieldType = "integer"
+    }
+}
+FGTilesJumpthru.placements = {
+    name = "FGTilesJumpthru",
+    data = {
+        width = 8,
+        texture = "wood",
+        surfaceIndex = 8
+    }
+}
+
+local function getTexture(entity)
+    return entity.texture and entity.texture ~= "default" and entity.texture or "wood"
+end
+
+function FGTilesJumpthru.sprite(room, entity)
+    local textureRaw = getTexture(entity)
+    local texture = "objects/jumpthru/" .. textureRaw
+
+    local x, y = entity.x or 0, entity.y or 0
+    local width = entity.width or 8
+
+    local startX, startY = math.floor(x / 8) + 1, math.floor(y / 8) + 1
+    local stopX = startX + math.floor(width / 8) - 1
+    local len = stopX - startX
+
+    local sprites = {}
+
+    for i = 0, len do
+        local quadX = 8
+        local quadY = 8
+
+        if i == 0 then
+            quadX = 0
+            quadY = room.tilesFg.matrix:get(startX - 1, startY, "0") ~= "0" and 0 or 8
+
+        elseif i == len then
+            quadY = room.tilesFg.matrix:get(stopX + 1, startY, "0") ~= "0" and 0 or 8
+            quadX = 16
+        end
+
+        local sprite = drawableSpriteStruct.fromTexture(texture, entity)
+
+        sprite:setJustification(0, 0)
+        sprite:addPosition(i * 8, 0)
+        sprite:useRelativeQuad(quadX, quadY, 8, 8)
+
+        table.insert(sprites, sprite)
+    end
+
+    return sprites
+end
+
+function FGTilesJumpthru.selection(room, entity)
+    return utils.rectangle(entity.x, entity.y, entity.width, 8)
+end
+
+return FGTilesJumpthru
